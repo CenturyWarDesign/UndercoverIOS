0e6d33ddeb6466d58fc3b3113f3be4e453b33245
jlcao <cjl_2029@163.com>
2014-08-19 23:23:58 +0800

    有胆你就点

---
+++ UnderCoverIOS/UnderCoverIOS/UCITouchButtonViewController.h
@@ -0,0 +1,13 @@
+//
+//  UCITouchButtonViewController.h
+//  UnderCoverIOS
+//
+//  Created by jlcao on 14-8-19.
+//  Copyright (c) 2014年 CenturyGame. All rights reserved.
+//
+
+#import "UCIBaseViewController.h"
+
+@interface UCITouchButtonViewController : UCIBaseViewController
+
+@end
